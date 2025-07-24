ddb70e5302736cd8b3a3a4f24bfab9a76da0cf20
mohammed ali <hasoon7891@hotmail.com>
2025-07-24 15:57:22 +0300

    commit message goes here

---
+++ homework/controllers/dataController.js
@@ -0,0 +1,64 @@
+const Log = require('../models/log.js');
+
+const dataController = {}
+dataController.index = async (req,res,next) => {
+   try {
+    res.locals.data.logs = await Log.find({})
+    next()
+   } catch(error) {
+    res.status(400).send({ message: error.message })
+  }
+}
+
+dataController.destroy = async (req, res, next ) => {
+    try {
+         await Log.findOneAndDelete({'_id': req.params.id }).then(() => {
+            next()
+         })
+    } catch (error) {
+      res.status(400).send({ message: error.message })
+    }
+}
+
+dataController.update = async (req, res, next) => {
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else if(req.body.readyToEat !== true) {
+        req.body.readyToEat = false;
+    }
+    try {
+      res.locals.data.log = await Log.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      next()
+    } catch (error) {
+      res.status(400).send({ message: error.message })
+    }
+}
+
+dataController.create = async (req, res, next) => {
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else if(req.body.readyToEat !== true) {
+        req.body.readyToEat = false;
+    }
+    try {
+      res.locals.data.log = await Log.create(req.body)
+      next()
+    } catch (error) {
+      res.status(400).send({ message: error.message })
+    }
+}
+
+dataController.show = async (req, res, next) => {
+    try {
+        res.locals.data.log = await Log.findById(req.params.id)
+        if(!res.locals.data.log){
+            throw new error(`could not locate a log with the id ${req.params.id}`)
+        }
+        next()
+    } catch (error) {
+      res.status(400).send({ message: error.message })
+    }
+}
+
+
+module.exports = dataController
---
+++ homework/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const Log = require('./models/log')
+const jsxEngine = require('jsx-view-engine')
+const methodOverride = require('method-override')
+const LogsRouter = require('./controllers/routeController')
+const PORT = process.env.PORT || 3000
+
+
+app.set('view engine', 'jsx')
+app.engine('jsx', jsxEngine())
+
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo')
+})
+
+
+
+// middleware to give us the body of the request data the user filled out
+app.use(express.urlencoded({ extended: true })) // req.body
+app.use(methodOverride('_method')) // <====== add method override
+app.use(express.static('public'))
+
+app.use((req,res,next)=> {
+    res.locals.data = {}
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+    console.log('app running on port 3000 you are so good at coding...')
+})
